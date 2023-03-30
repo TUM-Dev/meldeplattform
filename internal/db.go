654a2429a223eaa063eeac77050fe5b109812858
Joscha Henningsen <joscha.henningsen@tum.de>
2023-03-30 13:13:36 +0200

    use files dir for db

--- internal/db.go
+++ internal/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (a *App) initDB() error {
-	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("files/database.db"), &gorm.Config{})
 	if err != nil {
 		return fmt.Errorf("open database: %w", err)
 	}
